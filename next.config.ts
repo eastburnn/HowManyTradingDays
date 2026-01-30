07c89adc72c5f6a91a07356ad37aac2cae58f428
eastburnn <chrris.ray@gmail.com>
2026-01-29 22:28:35 -0500

    revert

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.howmanytradingdays.com",
-          },
-        ],
-        destination: "https://howmanytradingdays.com/:path*",
-        permanent: true, // 308
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
